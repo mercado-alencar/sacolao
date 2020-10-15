c2ea1907a45108f36cd6d898a5405aafb098ac58
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-10-15 07:54:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,10 +19,12 @@ app.use(function(req, res, next) {
   }
   next();
 });
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
   res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
            
   if (req.method === 'OPTIONS') {

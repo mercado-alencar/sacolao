119fa4320c7aa59acd8fdb18d27b8caca8ffb3c6
Alan Balen Schio <alanbschio@gmail.com>
2021-01-16 16:32:15 -0400

    fix:  method calling

--- src/router/VendaRouter.js
+++ src/router/VendaRouter.js
@@ -7,7 +7,7 @@ this.service = new VendaService();
 router.post('/', async (req, res) => {
 	let dto = req.body;
 
-	let entity = await this.service.save(dto);
+	let entity = await this.service.create(dto);
 	if (entity.erro) {
 		res.status(500).send(entity);
 	} else
